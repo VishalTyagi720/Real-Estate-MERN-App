bb67941b8e9dc41a5e803056f56e6a44f45fe0d4
VishalTyagi720 <110998294+VishalTyagi720@users.noreply.github.com>
2024-02-28 22:10:00 +0530

    Basic frontend setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,10 @@
+// import React from 'react'
+
+function App() {
+  return (
+    <div className='text-red-500'>App</div>
+  )
+}
+
+
+export default App;
---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", //Add your files here to include them in the Tailwind CSS pipeline. All the file with these extension will be detectedand css will be applied.
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
