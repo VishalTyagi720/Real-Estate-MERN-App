cb6c7db8123c7948bcd38e677db4426f847898b0
VishalTyagi720 <110998294+VishalTyagi720@users.noreply.github.com>
2024-03-02 15:26:08 +0530

    Database connrction

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_PASSWORD)
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((error) => {
+    console.log(error);
+})
 
 
 const app = express();
@@ -7,3 +21,13 @@ const app = express();
 app.listen('3000', () => {
     console.log('Server is running on port 3000!!!');
 })
+
+
+
+
+
+
+
+
+// username : vishalsinghx00
+// password : Vishal752002
